3ca036618ff8cb9ab6f968d5a727e16de0086e75
saadzahid66 <saad.zahid66@gmail.com>
2022-08-14 22:55:39 +0500

    Added ability to fetch unique column sinto selectbox from dataset

--- Generic_AnalyticsWebApp/DataVisualizer_Generic.py
+++ Generic_AnalyticsWebApp/DataVisualizer_Generic.py
@@ -19,11 +19,6 @@ data_display_text = "The columns, rows & values were not formatted or truncated 
 
 lottie_anim_hello = 'https://assets9.lottiefiles.com/packages/lf20_49rdyysj.json'
 
-options_functions = (   "main.py (API)  main", "main.py (API)  save_to_db", "main.py (API)  collection_exists", "main.py (API)  db_get_newest_entry", "main.py (API)  read_from_db", "main.py (API)  recommendation", "main.py (API)  ai_cosin_sim", "main.py (API)  read_from_db_by_date", "main.py (API)  db_get_articles_by_url",
-                        "get_newest_articles_api.py get_keywords", "get_newest_articles_api.py save_to_db", "get_newest_articles_api.py db_check_not_empty", "get_newest_articles_api.py db_get_newest_entry", "get_newest_articles_api.py get", "get_newest_articles_api.py main", 
-                        "get_ge_data.py ga_api", "get_ge_data.py save_to_db", "get_ge_data.py main",
-                        "daily_sdct.py (categories and keywords)  save_to_db", "daily_sdct.py (categories and keywords)  read_from_db"  
-                    )
 
 
 # --- Utility Functions --- 
@@ -112,7 +107,7 @@ with st.sidebar:
     st.title("streamlit - Data Visualizer")
     st.markdown("##")
 
-    st.header(":page_facing_up: Upload File")
+    st.header(":open_file_folder: Upload File")
     df_uploaded = upload_data()
 
 
@@ -175,19 +170,14 @@ if df_uploaded is not None:
 if df_uploaded is not None:
 
     with st.sidebar:
-        st.header(":page_facing_up: Select Function")
+        st.header(":open_file_folder: Select Function")
 
-        selected_function = st.selectbox( "Please select a function for analysis...", options_functions )
+        selected_function = st.selectbox( "Please select a function for analysis...", df_uploaded["Function"].unique() )
     
         df_selected_function = load_selected_function(selected_function)
 
 
     st.header(":bar_chart: Filter Data By Function")
-
-    state_selected_function = st.text("Please Select A Function From List To Analyze...")
-    selected_function = st.selectbox( " ", options_functions )
-    
-    df_selected_function = load_selected_function(selected_function)
 
 
 # Data display & overall metrics display
