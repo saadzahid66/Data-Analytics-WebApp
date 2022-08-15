ad8d60a7181281bdf357d860b0a7fd203bfe2100
saadzahid66 <saad.zahid66@gmail.com>
2022-08-15 11:33:07 +0500

    file path and comment

--- Generic_AnalyticsWebApp/DataVisualizer_Generic.py
+++ Generic_AnalyticsWebApp/DataVisualizer_Generic.py
@@ -7,13 +7,12 @@ from plotly import graph_objs as go
 from PIL import Image as img
 
 
-
 # --- Helpful Variables ---
 
-# Pre-written List of Functions
-title_dashboard_logo ='D:\Work\Python\Data Analytics WebApp\.resources\dash_small.png'
-sidebar_streamlit_logo = 'D:\Work\Python\Data Analytics WebApp\.resources\streamlit-logo-nbg.png'
-main_waiting_logo = 'D:\Work\Python\Data Analytics WebApp\.resources\Waiting.png'
+# Image Oaths
+title_dashboard_logo = '.\Generic_AnalyticsWebApp\dash_small.png'
+sidebar_streamlit_logo = '.\Generic_AnalyticsWebApp\streamlit-logo-nbg.png'
+main_waiting_logo = '.\Generic_AnalyticsWebApp\Waiting.png'
 
 information_text = "This web application designed to present & visualize performance metrics of specific CSV data set ('merged_sorted.csv') present in the Git repo. To view the data in full-screen please click the button on top-right of each widget.  Your feedback is much awaited. Thankyou for giving the opprotunity to do something new."
 data_display_text = "The columns, rows & values were not formatted or truncated to keep dataset authenticity. However, the data-set was altered to retain only useful information & decrease load times. Also, not HTML CSS elements were used to keep the usage of streamlit as a priority."
@@ -21,30 +20,34 @@ data_display_text = "The columns, rows & values were not formatted or truncated 
 lottie_anim_hello = 'https://assets9.lottiefiles.com/packages/lf20_49rdyysj.json'
 
 
-
-# --- Utility Functions --- 
+# --- Utility Functions ---
 
 # File Uploading Section
 def upload_data():
-    file = st.file_uploader("Please upload csv file to continue...", type="csv")
+    file = st.file_uploader(
+        "Please upload csv file to continue...", type="csv")
     try:
-        df = pd.read_csv( file )
+        df = pd.read_csv(file)
         return df
     except Exception as e:
         st.text(f"")
 
 # Filters & displays data according to selected function
+
+
 def load_selected_function(selected_function):
     df_selected_function = df_uploaded.query("Function == @selected_function")
 
     return df_selected_function
 
 # Calculates meta data for overall performance (Data unformatted to keep authenticity)
+
+
 def calculate_metadata(df_any):
-    total_time = int( df_any['Time'].sum() )
-    min_time = float( df_any['Time'].min() )
-    max_time = float( df_any['Time'].max() )
-    avg_time = float( df_any['Time'].mean() )
+    total_time = int(df_any['Time'].sum())
+    min_time = float(df_any['Time'].min())
+    max_time = float(df_any['Time'].max())
+    avg_time = float(df_any['Time'].mean())
 
     st.text(f"  Total Time:    {total_time} s")
     st.text(f"Average Time:    {avg_time} s")
@@ -52,59 +55,69 @@ def calculate_metadata(df_any):
     st.text(f"Minimum Time:    {min_time} s")
 
 # Plots min,max,mean,sum of each function in the entire uploaded data-set
-def plot_uploaded_data(df_uploaded, op_choice):
-    if op_choice==1: df_grouped_function_time = df_uploaded.groupby( by=['Function'] ).max()[ ['Time'] ].sort_values( by=['Function'] )
-    elif op_choice==2: df_grouped_function_time = df_uploaded.groupby( by=['Function'] ).min()[ ['Time'] ].sort_values( by=['Function'] )
-    elif op_choice==3: df_grouped_function_time = df_uploaded.groupby( by=['Function'] ).max()[ ['Time'] ].sort_values( by=['Function'] )
-    elif op_choice==4: df_grouped_function_time = df_uploaded.groupby( by=['Function'] ).max()[ ['Time'] ].sort_values( by=['Function'] )
 
-    fig_grouped_function_time = px.bar( df_grouped_function_time, x='Time', y= df_grouped_function_time.index, orientation='h', color_discrete_sequence=["#0083B8"] * len(df_grouped_function_time) , template='plotly_white' )
-    fig_grouped_function_time.layout.update( xaxis_rangeslider_visible=True )
+
+def plot_uploaded_data(df_uploaded, op_choice):
+    if op_choice == 1:
+        df_grouped_function_time = df_uploaded.groupby(by=['Function']).max()[
+            ['Time']].sort_values(by=['Function'])
+    elif op_choice == 2:
+        df_grouped_function_time = df_uploaded.groupby(by=['Function']).min()[
+            ['Time']].sort_values(by=['Function'])
+    elif op_choice == 3:
+        df_grouped_function_time = df_uploaded.groupby(by=['Function']).max()[
+            ['Time']].sort_values(by=['Function'])
+    elif op_choice == 4:
+        df_grouped_function_time = df_uploaded.groupby(by=['Function']).max()[
+            ['Time']].sort_values(by=['Function'])
+
+    fig_grouped_function_time = px.bar(df_grouped_function_time, x='Time', y=df_grouped_function_time.index, orientation='h', color_discrete_sequence=[
+                                       "#0083B8"] * len(df_grouped_function_time), template='plotly_white')
+    fig_grouped_function_time.layout.update(xaxis_rangeslider_visible=True)
 
     st.plotly_chart(fig_grouped_function_time)
 
 # Plots the data for the selected function from the select-box
+
+
 def plot_selected_data(df_selected_function):
     fig_selected_function = go.Figure()
-    fig_selected_function.add_trace( go.Scatter( x=df_selected_function['time_stamp'], y=df_selected_function['Time'], name='time(s)' ) )
-    fig_selected_function.layout.update( xaxis_rangeslider_visible=True, xaxis=(dict(showgrid=False)), yaxis=(dict(showgrid=False)), plot_bgcolor="rgba(0,0,0,0)",   )
+    fig_selected_function.add_trace(go.Scatter(
+        x=df_selected_function['time_stamp'], y=df_selected_function['Time'], name='time(s)'))
+    fig_selected_function.layout.update(xaxis_rangeslider_visible=True, xaxis=(dict(
+        showgrid=False)), yaxis=(dict(showgrid=False)), plot_bgcolor="rgba(0,0,0,0)",)
 
-    st.plotly_chart(fig_selected_function, use_container_width=True )
-
+    st.plotly_chart(fig_selected_function, use_container_width=True)
 
 
 # --- WEB PAGE SETTINGS & MAIN CODE ---
 
 # Page Layout Settings
-st.set_page_config( page_title='Data Visualizer' , page_icon=':bar_chart:', layout='wide' )
+st.set_page_config(page_title='Data Visualizer',
+                   page_icon=':bar_chart:', layout='wide')
 
-left_column_page, right_column_page= st.columns(2)
+left_column_page, right_column_page = st.columns(2)
 with left_column_page:
     st.title(":bar_chart: Data Visualizer")
     st.markdown(information_text)
     st.markdown(data_display_text)
-    
-    
+
 
 with right_column_page:
     dashboard_logo = img.open(title_dashboard_logo)
     st.image(dashboard_logo)
 
-    
-    
-
 
 # File Upload Section
 with st.sidebar:
     streamlit_logo = img.open(sidebar_streamlit_logo)
-    
+
     st.image(streamlit_logo)
     st.title("streamlit - Data Visualizer")
     st.markdown("##")
 
     st.header(":open_file_folder: Upload File")
     df_uploaded = upload_data()
-
 
 
 # Data display & overall metrics display
@@ -129,7 +142,7 @@ else:
 
     st.subheader("Waiting For File...!")
     st.image(waiting_logo)
-    #st_lottie(load_lottie(lottie_anim_hello), key='hello') Note: doesnt work with url or json
+    # st_lottie(load_lottie(lottie_anim_hello), key='hello') Note: doesnt work with url or json
 
 
 # Data plot of Maximum & Minimum time taken by each function
@@ -157,7 +170,6 @@ if df_uploaded is not None:
         st.subheader(":chart_with_upwards_trend:Mean Time Of Each Function")
         plot_uploaded_data(df_uploaded, 4)
 
-
     st.markdown("---")
 
 
@@ -167,10 +179,10 @@ if df_uploaded is not None:
     with st.sidebar:
         st.header(":open_file_folder: Select Function")
 
-        selected_function = st.selectbox( "Please select a function for analysis...", df_uploaded["Function"].unique() )
-    
-        df_selected_function = load_selected_function(selected_function)
+        selected_function = st.selectbox(
+            "Please select a function for analysis...", df_uploaded["Function"].unique())
 
+        df_selected_function = load_selected_function(selected_function)
 
     st.header(":bar_chart: Filter Data By Function")
 
@@ -189,13 +201,9 @@ if df_uploaded is not None:
 
         calculate_metadata(df_selected_function)
 
-
     # Data plot of Function;s Time vs Timestamp
 if df_uploaded is not None:
     st.subheader(":chart_with_upwards_trend:Time vs TimeStamp")
     plot_selected_data(df_selected_function)
 
     st.markdown("---")
-
-
-
